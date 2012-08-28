9cac46ab327594ffb23406767823e7b8b6623837
skiyo <skiyo@me.com>
2012-08-28 17:29:55 +0800

    update

--- src/model/GithubPress.class.php
+++ src/model/GithubPress.class.php
@@ -7,4 +7,24 @@
  */
 class GithubPress {
 	
+	public function isLogin() {
+		$user_github_id = Cookie::get('gpu');
+		
+	}
+	
+	/*
+	public function regist() {
+		
+	}
+	*/
+	
+	public function login($access_token) {
+		$api = GithubAPI::getInstance();
+		$user_info = $api->getUserInfo($access_token);
+		$email = $api->getEmails($access_token);
+		//Cookie::set('gpu', $user_info['']);
+	}
+	
+	
+	
 }
